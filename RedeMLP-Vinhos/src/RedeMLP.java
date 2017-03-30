dde4a44bfb6ee5770a7c62a32f4ac8773f6de579
grschroder <schroder.guilherme@gmail.com>
2017-03-30 10:53:59 -0300

    Update

--- RedeMLP-Vinhos/src/RedeMLP.java
+++ RedeMLP-Vinhos/src/RedeMLP.java
@@ -4,7 +4,10 @@ public class RedeMLP {
 	public static void main(String[] args) {
 		// Normalizar as entradas para estarem entre 0 e 1.
 		// entradas que devem ser normalizadas: fixed acidity, residual sugar, free sulfur dioxide, total sulfur dioxide, pH, sulphates, alcohol
+		//densidade vem bugada no excel e no libreoffice
 		// a saída do programa é a quality
+		
+		
 		
 		
 	}
