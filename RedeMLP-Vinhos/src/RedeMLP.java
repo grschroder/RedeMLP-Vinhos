da41220a7eb2bebbe25f7e666cf6183c7eb9b843
grschroder <schroder.guilherme@gmail.com>
2017-03-29 23:09:52 -0300

    Update

--- RedeMLP-Vinhos/src/RedeMLP.java
+++ RedeMLP-Vinhos/src/RedeMLP.java
@@ -1,9 +1,12 @@
 
-public class Main {
+public class RedeMLP {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Normalizar as entradas para estarem entre 0 e 1.
+		// entradas que devem ser normalizadas: fixed acidity, residual sugar, free sulfur dioxide, total sulfur dioxide, pH, sulphates, alcohol
+		// a saída do programa é a quality
+		
+		
 	}
 
 }
