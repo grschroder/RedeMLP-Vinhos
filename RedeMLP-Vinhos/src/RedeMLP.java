84016e90a1b60984d3c0a7bb6dd8516b49f42918
grschroder <schroder.guilherme@gmail.com>
2017-03-31 20:15:07 -0300

    Update

--- RedeMLP-Vinhos/src/RedeMLP.java
+++ RedeMLP-Vinhos/src/RedeMLP.java
@@ -7,6 +7,18 @@ public class RedeMLP {
 		//densidade vem bugada no excel e no libreoffice
 		// a saída do programa é a quality
 		
+		double x1[] = new double[2];
+		double x2[] = new double[2];
+		double x3[] = new double[2];
+		double x4[] = new double[2];
+		double x5[] = new double[2];
+		double x6[] = new double[2];
+		double x7[] = new double[2];
+		double x8[] = new double[2];
+		double x9[] = new double[2];
+		double x10[] = new double[2];
+		double x11[] = new double[2];
+		double x12[] = new double[2];
 		
 		
 		
